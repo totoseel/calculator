2300d44ba4f4b593b69aa3ba9d5a6bb270376888
totoseel <ojinsup@naver.com>
2026-05-28 13:50:40 +0900

    [Refactor] Rename argument, Extract  Setup

--- src/main/java/org/example/baseball/Game.java
+++ src/main/java/org/example/baseball/Game.java
@@ -1,8 +1,8 @@
 package org.example.baseball;
 
 public class Game {
-    public void guess(Object o) {
-        if (o == null) {
+    public void guess(Object guessNumber) {
+        if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/org/example/baseball/GameTest.java
+++ src/test/java/org/example/baseball/GameTest.java
@@ -1,16 +1,21 @@
 package org.example.baseball;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class GameTest {
-    Game game = new Game();
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
 
     @Test
     void createGame() {
-        Game game = new Game();
         assertThat(game).isNotNull();
     }
 
