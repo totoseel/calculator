f64b51ec9d2fa1688d2b56df2aefed07586ea781
totoseel <ojinsup@naver.com>
2026-05-28 17:03:05 +0900

    [REFACTOR] Extract MAX_SCORE constant in AlphaSimilarityChecker

--- src/main/java/org/example/similaritycheckers/AlphaSimilarityChecker.java
+++ src/main/java/org/example/similaritycheckers/AlphaSimilarityChecker.java
@@ -2,7 +2,9 @@ package org.example.similaritycheckers;
 
 public class AlphaSimilarityChecker {
 
+    private static final int MAX_SCORE = 40;
+
     public int calculate(String a, String b) {
-        return 40;
+        return MAX_SCORE;
     }
 }
