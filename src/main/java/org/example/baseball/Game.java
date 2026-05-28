9374a8772e16f993c896e0416fe650ab7c947ab7
totoseel <ojinsup@naver.com>
2026-05-28 14:04:11 +0900

    [Green] Add Inspection Rule - duplicated numbers

--- src/main/java/org/example/baseball/Game.java
+++ src/main/java/org/example/baseball/Game.java
@@ -11,9 +11,15 @@ public class Game {
         }
 
         for (char number : guessNumber.toCharArray()) {
-            if (number < '0' ||  number > '9') {
+            if (number < '0' || number > '9') {
                 throw new IllegalArgumentException();
             }
+        }
+
+        if (guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(1) == guessNumber.charAt(2)
+                || guessNumber.charAt(0) == guessNumber.charAt(2)) {
+            throw new IllegalArgumentException();
         }
     }
 }
--- src/test/java/org/example/baseball/GameTest.java
+++ src/test/java/org/example/baseball/GameTest.java
@@ -1,10 +1,10 @@
 package org.example.baseball;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 class GameTest {
     private Game game;
@@ -25,13 +25,14 @@ class GameTest {
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
         assertIllegalArgument("12s");
+        assertIllegalArgument("121");
     }
 
     private void assertIllegalArgument(String guessNum) {
         try {
             game.guess(guessNum);
             fail("");
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
 
         }
     }
