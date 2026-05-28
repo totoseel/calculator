9f9766cf52a4b9ac5f28a7b060ddd1d9922dc59e
totoseel <ojinsup@naver.com>
2026-05-28 10:13:34 +0900

    [REFACTOR] Method extraction for readability and Add Test Cases

--- src/main/java/org/example/PrimeFactors.java
+++ src/main/java/org/example/PrimeFactors.java
@@ -6,10 +6,20 @@ import java.util.List;
 public class PrimeFactors {
     public List<Integer> calculate(int number){
         List<Integer> factors = new ArrayList<>();
-        if (number < 2) {
+        if (isInvalidInput(number)) {
             return factors;
         }
 
+        findPrimeFactor(number, factors);
+
+        return factors;
+    }
+
+    private boolean isInvalidInput(int number) {
+        return number < 2;
+    }
+
+    private void findPrimeFactor(int number, List<Integer> factors) {
         for (int i = 2; i * i <= number; i++) {
             while (number % i == 0) {
                 factors.add(i);
@@ -20,7 +30,5 @@ public class PrimeFactors {
         if (number > 1) {
             factors.add(number);
         }
-
-        return factors;
     }
 }
--- src/test/java/org/example/PrimeFactorsTest.java
+++ src/test/java/org/example/PrimeFactorsTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class PrimeFactorsTest {
     PrimeFactors primeFactors = new PrimeFactors();
@@ -18,6 +20,24 @@ class PrimeFactorsTest {
         List<Integer> calculate_4 = primeFactors.calculate(4);
         Assertions.assertEquals(2, calculate_4.getFirst());
         Assertions.assertEquals(2, calculate_4.get(1));
+
+
+        List<Integer> calculate_15 = primeFactors.calculate(15);
+        Assertions.assertEquals(3, calculate_15.getFirst());
+        Assertions.assertEquals(5, calculate_15.get(1));
+
+        List<Integer> calculate_17 = primeFactors.calculate(17);
+        Assertions.assertEquals(17, calculate_17.getFirst());
+
+        List<Integer> calculate_23 = primeFactors.calculate(23);
+        Assertions.assertEquals(23, calculate_23.getFirst());
+
+
+        List<Integer> calculate_126 = primeFactors.calculate(126);
+        Assertions.assertEquals(2, calculate_126.getFirst());
+        Assertions.assertEquals(3, calculate_126.get(1));
+        Assertions.assertEquals(3, calculate_126.get(2));
+        Assertions.assertEquals(7, calculate_126.get(3));
     }
 
     @Test
@@ -25,5 +45,11 @@ class PrimeFactorsTest {
         Assertions.assertEquals(0, primeFactors.calculate(-1).size());
         Assertions.assertEquals(0, primeFactors.calculate(0).size());
         Assertions.assertEquals(0, primeFactors.calculate(1).size());
+    }
+
+    @Test
+    void testUnder100() {
+
+
     }
 }
