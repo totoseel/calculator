ed2e3f29ce16d223308a0844af4e9c985757a537
totoseel <ojinsup@naver.com>
2026-05-28 14:01:22 +0900

    [Green] Throw exception when guessNum is not int

--- src/test/java/org/example/baseball/GameTest.java
+++ src/test/java/org/example/baseball/GameTest.java
@@ -24,6 +24,7 @@ class GameTest {
         assertIllegalArgument(null);
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
+        assertIllegalArgument("12s");
     }
 
     private void assertIllegalArgument(String guessNum) {
